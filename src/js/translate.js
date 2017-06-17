a54bd9697933f8dd9c9cca31e1d57474a4816b12
Vadym <vadoost@gmail.com>
2017-06-17 21:15:57 +0300

    added translate langs

--- src/js/translate.js
+++ src/js/translate.js
@@ -1,1 +1,49 @@
+(function () {
+    var dropDown = document.getElementById('dropDown');
+    var langBtns = document.querySelectorAll('[langBtn]');
+    langBtns.forEach(function (btn) {
+        btn.onclick = changeLang;
+    });
+    var data = LANG_EN;
+    var userLang = navigator.language || navigator.userLanguage;
+    var lang = userLang.slice(0,2);
+    var translateElems = document.querySelectorAll('[translate]');
 
+    // toTranslate(lang);
+    toTranslate('ru');
+
+    function toTranslate(lang) {
+        if (lang === 'ru')
+            data = LANG_RU;
+        else
+            data = LANG_EN;
+
+        translateElems.forEach(function (elem) {
+            var keys = elem.getAttribute('translate').split('.');
+            var langObj = data;
+            keys.forEach(function (key) {
+                langObj = langObj[key];
+            });
+            elem.innerHTML = langObj;
+        });
+    }
+
+    function changeLang(e) {
+        toTranslate(e.target.getAttribute('langBtn'));
+    }
+
+    function getTranslateJSON(lang) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', lang + '.json', true);
+        xhr.send();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState != 4) return;
+            if (xhr.status != 200) {
+                // обработать ошибку
+            } else {
+                // вывести результат
+            }
+        }
+    }
+
+})();
